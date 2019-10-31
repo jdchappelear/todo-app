0cf729172590a98038a881f85a0fa7722341fe8c
John Chappelear <john.chappelear@gmail.com>
2019-10-30 23:20:09 -0400

    After setup of 'location' of components in HTML files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { TodoItemComponent } from './todo-item/todo-item.component';
